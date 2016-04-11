f1434f2121779220e50fef66653a13fc671549f0
coreysiegel <crs54@cornell.edu>
2016-04-10 21:10:07 -0700

    utilize xml parser

--- csv2tcx.py
+++ csv2tcx.py
@@ -1,49 +1,49 @@
-def main():
-	try:
-		csv_import=False
-		pdb_import=False
-		import csv
-		csv_import=True
-		import pdb
-		pdb_import=True
-	except ImportError:
-		print('Import Error detected!')
-		print([['csv','pdb'],[csv_import,pdb_import]])
-		if not pdb_import:
-			return
+import re
+import sys
+import argparse
+import string
+import pdb
 
-	file='2014-07-06-08-08-54.tcx'
-	file_root=file[:-4]
+# takes in a TCX file and outputs a CSV file
+def main (input, output):
+	fin=open(input, 'r')
+	fout=open(output, 'w')
+	row_id=0
+	skipNext=False
+	for line in fin:
+		row_id+=1
+		s=line.strip().split(',')
+		if row_id==1:
+			columns=s
+			numColumns=len(columns)
+		else:
+			fout.write('<Trackpoint>\n')
+			for i in range(0, len(s)):
+				if skipNext:
+					skipNext=False
+				elif columns[i]=='LatitudeDegrees' or columns[i]=='LongitudeDegrees':
+					fout.write('<Position>\n'+ \
+							'<'+columns[i]+'>'+s[i]+'</'+columns[i]+'>\n'+ \
+							'<'+columns[i+1]+'>'+s[i+1]+'</'+columns[i+1]+'>\n'+ \
+							'</Position>\n')
+					skipNext=True
+				elif columns[i]=='heartratebpm/value':
+					fout.write('<HeartRateBpm>\n')
+					fout.write('<Value>'+s[i]+'</Value>\n')
+					fout.write('</HeartRateBpm>\n')
+				else:
+					fout.write('<'+columns[i]+'>'+s[i]+'</'+columns[i]+'>\n')
+			fout.write('</Trackpoint>\n')
+	fout.close()
+	fin.close()
 
-	time=0
-	ele=1
-	lat=2
-	long=3
-	hr=4
-
-	with open(file_root+'.csv',newline='') as csvfile:
-		with open(file,'w',newline='') as tcxfile:
-			reader=csv.reader(csvfile,delimiter=',',quotechar='\'')
-			for row in reader:
-				if row[0][0:4]=='2014':
-					tcxfile.write('          <Trackpoint>\n')
-					tcxfile.write('            <Time>'+row[time]+'</Time>\n')
-					if len(row[lat]) > 0 or len(row[long]) > 0:
-						tcxfile.write('            <Position>\n')
-						if len(row[lat]) > 0:
-							tcxfile.write('              <LatitudeDegrees>'+row[lat]+'</LatitudeDegrees>\n')
-						if len(row[long]) > 0:
-							tcxfile.write('              <LongitudeDegrees>'+row[long]+'</LongitudeDegrees>\n')
-						tcxfile.write('            </Position>\n')
-					if len(row[ele]) > 0:
-						tcxfile.write('            <AltitudeMeters>'+row[ele]+'</AltitudeMeters>\n')
-					if len(row[hr]) > 0:
-						tcxfile.write('            <HeartRateBpm xsi:type="HeartRateInBeatsPerMinute_t">\n')
-						tcxfile.write('              <Value>'+row[hr]+'/Value>\n')
-						tcxfile.write('            </HeartRateBpm>\n')
-					tcxfile.write('          </Trackpoint>\n')
-			tcxfile.close()
-		csvfile.close()
-
-if __name__ == '__main__':
-	main()
+if __name__=='__main__':
+	# arguments
+	parser = argparse.ArgumentParser(description=
+		'something.')
+	parser.add_argument('input', help='input CSV file')
+	parser.add_argument('output', help='output TCX file')
+	#parser.add_argument('--verbose', help='increase output verbosity', action='store_true')
+	args = parser.parse_args()
+	
+	main(args.input, args.output)
